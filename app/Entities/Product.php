4afac3c7ad15cf68c7b7b3ab75e6d66847dc6bd4
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2019-05-02 22:07:16 -0300

    Ajustado o preço dos produtos

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -11,4 +11,8 @@ class Product extends Model
         'price',
     ];
 
+    protected $casts = [
+        'price' => 'float',
+    ];
+
 }
--- app/Services/ProductServices.php
+++ app/Services/ProductServices.php
@@ -12,49 +12,5 @@ class ProductServices extends DefaultServices
         $this->entity = Product::class;
     }
 
-    public function create($request)
-    {
-
-        $data = $request->all();
-
-        $data['price'] = str_replace(',', '.', (str_replace('.', '', $data['price'])));
-
-        $result = $this->entity::create($data);
-
-        if (request()->wantsJson()) {
-            return $result;
-        }
-
-        $response = [
-            'message' => 'Created.',
-        ];
-
-        return redirect()->back()->with('success', $response['message']);
-
-    }
-
-    public function update($request, $id)
-    {
-
-        $data = $request->all();
-
-        $result = $this->entity::where('id', $id)->first();
-
-        $data['price'] = str_replace(',', '.', (str_replace('.', '', $data['price'])));
-
-        $result->update($data);
-
-        if (request()->wantsJson()) {
-            return $result;
-        }
-
-        $response = [
-            'message' => 'Updated.',
-        ];
-
-        return redirect()->back()->with('success', $response['message']);
-
-    }
-
 }
 
